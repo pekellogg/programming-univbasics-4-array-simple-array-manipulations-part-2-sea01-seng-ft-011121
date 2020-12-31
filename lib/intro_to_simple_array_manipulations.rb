9c51845cd091559ba378bf30da35949e7b01bcde
Peyton Kellogg <pekellogg@hotmail.com>
2020-12-31 03:17:48 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,8 +1,9 @@
-# blue = ["a", "b"]
-# green = ["c", "d"]
 
 def using_concat(anything, array2)
   anything.concat(array2)
 end
 
-# using_concat(blue, green)
+def using_insert(array, new_element)
+  new_element.insert(array[4])
+end
+
